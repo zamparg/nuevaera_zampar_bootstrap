1816b507426e954b02de5a854508099aa50bcef0
zamparg <zamparg@hotmail.com>
2022-01-22 13:43:11 -0300

    corrección de archivos php para funcionamiento de formularios

--- php/envio-contacto.php
+++ php/envio-contacto.php
@@ -4,8 +4,6 @@ use PHPMailer\PHPMailer\Exception;
 require 'Exception.php';
 require 'PHPMailer.php';
 require 'SMTP.php';
-
-
 $mail = new PHPMailer();
 
 $mail->IsSMTP();                                      // set mailer to
@@ -21,15 +19,24 @@ $nombre = $_POST['nombre'];
 $telefono = $_POST['telefono'];
 $email = $_POST['email'];
 $barrio = $_POST['barrio'];
-$presupuesto = $_POST['presupuesto'];
+$servicio = $_POST['servicio'];
+$otrostrabajos = $_POST['encuesta'];
+$presupuesto = $_POST['comentario'];
+$archivo = $_FILES['archivo'];
+
 $message = "<b>Nueva solicitud de contacto enviada el:</b>" . date('d/m/Y', time()) . "<br><br>" ;
 $message .= "<b>Nombre:</b> " . $nombre . " \r\n <br>";
 $message .= "<b>E-mail: </b>" . $email . " \r\n <br>";
 $message .= "<b>Teléfono: </b>" . $telefono . " \r\n <br>";
 $message .= "<b>Vive en: </b>" . $barrio . " \r\n <br>";
+$message .= "<b>¿Ya realizó trabajos con nosotros?: </b>" . $otrostrabajos . " \r\n <br>";
+$message .= "<b>Solicita: </b>" . $servicio . " \r\n <br>";
 $message .= "<b>Mensaje: </b>" . $presupuesto . " \r\n <br>";
 
-
+if(isset($_POST["submit"]))
+{
+$path = 'upload/' . $_FILES["archivo"]["name"];
+move_uploaded_file($_FILES["archivo"]["tmp_name"], $path); }
 
 //FIN DE PRUEBA
 
@@ -47,6 +54,8 @@ $mail->IsHTML(true);     // set email
 $mail->Subject = "Nueva solicitud de Presupuesto";
 $mail->Body    = utf8_decode($message);
 $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+$mail->AddAttachment($path);
+
 
 if(!$mail->Send())
 {
